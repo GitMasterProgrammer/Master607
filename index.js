01c0cb7dc14fcc93fa82935eab05d3a4f5219dce
GitMasterProgrammer <118828179+GitMasterProgrammer@users.noreply.github.com>
2023-10-20 17:52:44 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
 const nodemailer = require("nodemailer");
 const program = require("commander");
+const { promises } = require("dns");
+const fs = require(
+  "fs"
+).promises;
 
 program
   .option("--email <email>", "Sender's email address")
   .option("--password <password>", "Sender's email password or App Password")
   .option("--message <message>", "Email message")
-  .option("--subject <subject> (optional, default: 'Dzień dobry')")
+  .option("--subject <subject>", "Email subject (optional, default: 'Dzień dobry')", "Dzień dobry")
   .option("--receiver_email_core <receiver_email_core>", "Receiver's core email address, for example Master{generate}@wp.pl")
-  .option("--min_value <min_value>", "Mininmum value to generate receiver email")
-  .option("--max_value <max_value>", "Maximum value to generate receiver email")
+  .option("--min_value <min_value>", "Minimum value to generate receiver email", "0")
+  .option("--max_value <max_value>", "Maximum value to generate receiver email", "600")
   .parse(process.argv);
 
-const { client_email, client_password, message, subject, receiver_email_core, min_value, max_value } = program;
+var { email, password, message, subject, receiver_email_core, min_value, max_value } = program._optionValues;
 
-if (!client_email || !client_password || !message || !receiver_email_core) {
+
+/*if (!client_email || !client_password || !message || !receiver_email_core) {
   console.error("Usage: node app.js --email <your-email> --password <your-password> --message <your-message> --subject <subject> --receiver_email_core <receiver_email_core> [--min_value <min_value>] [--max_value <max_value>]");
   process.exit(1);
-}   
-
-if(!subject){
-    subject = "Dzień dobry";
-}
-
-if(!min_value){
-    min_value = 0;
-}
-
-if(!max_value){
-    max_value = 600;
-}
-
+}   */
 
 function generateMailOptions(email_client, email_core, subject, current_number, message){
     const regex = /{generate}/i;
@@ -64,9 +56,10 @@ function sendMail(email, password, mailOptions, number, max_number){
 }
 
 function MainActivity(min, max, subject, message, email, password, email_core){
+console.log(min, max, subject, message, email, password, email_core)
     for(let i=min; i<=max; i++){
         sendMail(email, password, generateMailOptions(email, email_core, subject, i, message), i, max)
     }
 }
 
-MainActivity(min_value, max_value, subject, message, client_email, client_password, receiver_email_core);
+MainActivity(min_value, max_value, subject, message, email, password, receiver_email_core);
